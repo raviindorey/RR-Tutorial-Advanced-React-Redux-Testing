2e4c96048fa63750872c0b14424691a6ed946e38
Ravi Indorey <ravi.indorey90@gmail.com>
2019-07-24 10:50:44 +0530

    Boilerplate

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => (
+  <div className="App">
+    Hello From App!
+  </div>
+);
+
+export default App;
