8d4084b2cf76708323c44f6f0cd663a5e187afcf
Ravi Indorey <ravi.indorey90@gmail.com>
2019-07-24 11:12:49 +0530

    Boilerplate

--- src/App.test.js
+++ src/App.test.js
@@ -4,8 +4,6 @@ import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
   expect(div.innerHTML).toContain('Hello From App!');
   ReactDOM.unmountComponentAtNode(div);
 });
-
