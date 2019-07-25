133987e7e161160129c8aa304fbcd082aff85e49
Ravi Indorey <ravi.indorey90@gmail.com>
2019-07-25 23:20:58 +0530

    Add test for CompomentList.jsx - CherrioWrapper

--- src/components/__tests__/CommentList.test.jsx
+++ src/components/__tests__/CommentList.test.jsx
@@ -24,3 +24,8 @@ afterEach(() => {
 it('creates on LI per comment', () => {
   expect(wrapped.find('li').length).toEqual(2);
 });
+
+it('shows comment for each comment', () => {
+  expect(wrapped.render().text()).toContain('Comment 1');
+  expect(wrapped.render().text()).toContain('Comment 2');
+});
