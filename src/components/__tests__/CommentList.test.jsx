0194d1cfebd498a145e67eeb6b0c29f7fd3af6ba
Ravi Indorey <ravi.indorey90@gmail.com>
2019-07-25 23:04:59 +0530

    Add test for CompomentList.jsx

---
+++ src/components/__tests__/CommentList.test.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { mount } from 'enzyme';
+
+import Root from 'Root.jsx';
+import CommentList from 'components/CommentList.jsx';
+
+// import { save_comment, saveComment } from 'actions';
+
+let wrapped;
+beforeEach(() => {
+  const initialState = { comments: ['Comment 1', 'Comment 2'] };
+
+  wrapped = mount(
+    <Root initialState={initialState}>
+      <CommentList />
+    </Root>,
+  );
+});
+
+afterEach(() => {
+  wrapped.unmount();
+});
+
+it('creates on LI per comment', () => {
+  expect(wrapped.find('li').length).toEqual(2);
+});
